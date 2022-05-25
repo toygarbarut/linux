10c0787d38af42c3de8d80f2467806cc0ac31f22
toygarbarut <toygarbarut@gmail.com>
2022-05-25 12:24:00 +0300

    The Project has been completed and updated.

--- projects/Project-101/Second_Part/user_passwd.sh
+++ projects/Project-101/Second_Part/user_passwd.sh
@@ -1,33 +1,53 @@
 #!/bin/bash
+#
+# This script creates a new user on the local system.
+# You will be prompted to enter the username (login), the person name, and a password.
+# The username, password, and host for the account will be displayed.
 
-if [[ ${UID} -ne 0 ]] 
+# Make sure the script is being executed with superuser privileges.
+if [[ "${UID}" -ne 0 ]]
 then
-        echo "please run with sudo command"
-        exit 1
+   echo 'Please run this script with sudo or as root.'
+   exit 1
 fi
 
-read -p 'Enter the name: ' USER_NAME
+# Get the username (login).
+read -p 'Enter the username to create: ' USER_NAME
 
-read -p 'Enter the comment: ' COMMENT
+# Get the real name (contents for the description field).
+read -p 'Enter the name of the person or application that will be using this account: ' COMMENT
 
-read -sp 'Enter the password: ' PASSWORD
+# Get the password.
+read -sp 'Enter the password to use for the account: ' PASSWORD
 
-useradd -c ${COMMENT} -m ${USER_NAME}
+# Create the account.
+useradd -c "${COMMENT}" -m ${USER_NAME} 2> /dev/null 
 
-if [[ $? -eq 0 ]]
+# Check to see if the useradd command succeeded.
+# We don't want to tell the user that an account was created when it hasn't been.
+if [[ "${?}" -ne 0 ]]
 then
-        echo -e  "\nuser $USER_NAME created succesfully."
-else
-        echo "Try again"
+  echo 'This username is already exit. Please select different username '
+  exit 1
 fi
 
+# Set the password.
 echo ${PASSWORD} | passwd --stdin ${USER_NAME}
 
-if [[ $? -eq 0 ]]
+# Check to see if the passwd command succeeded.
+if [[ "${?}" -ne 0 ]]
 then
-        echo -e  "\nPassword created succesfully."
-else
-        echo "Try again"
+  echo 'The password for the account could not be set.'
+  exit 1
 fi
 
+# Force password change on first login.
 passwd -e ${USER_NAME}
+
+# Display the username, password, and the host where the user was created.
+echo
+echo 'username:'
+echo "${USER_NAME}"
+echo
+echo 'password:'
+echo "${PASSWORD}"
